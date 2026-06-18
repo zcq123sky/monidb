51b9b932115f0a3a6d17dff858b05049ef9898c5
zhongchunquan <zcq123sky@icloud.com>
2026-06-19 06:54:16 +0800

    fix client

--- client.ts
+++ client.ts
@@ -2,7 +2,10 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import type { Sql } from "postgres";
 
-const databaseUrl: string = Deno.env.get("DATABASE_URL")!;
+const databaseUrl = Deno.env.get("DATABASE_URL");
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set");
+}
 const client: Sql = postgres(databaseUrl, { prepare: false });
 const dbClient: ReturnType<typeof drizzle> = drizzle(client);
 
