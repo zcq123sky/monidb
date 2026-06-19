f8d66a31bc2d4540060643cfc66336cef848e304
zhongchunquan <zcq123sky@icloud.com>
2026-06-19 12:13:36 +0800

    fix db link

--- client.ts
+++ client.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import type { Sql } from "postgres";
 
-const databaseUrl: string = Deno.env.get("DATABASE_URL")!;
+const databaseUrl = Deno.env.get("SUPABASE_DB_URL") ?? Deno.env.get("DATABASE_URL")!;
 const client: Sql = postgres(databaseUrl, { prepare: false });
 const db: ReturnType<typeof drizzle> = drizzle(client);
 
